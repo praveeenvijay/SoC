70722fb60248fa88600ff490974003919f4aeff7
Praveen Vijay <praveenvijayias@gmail.com>
2023-06-05 06:10:20 +0530

    updating after every time step

--- week1_multi_arm_bandit.py
+++ week1_multi_arm_bandit.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 
 def gaussian_arm():
     value = np.random.normal(2, 1)
-
     return value
 
 
@@ -43,12 +42,12 @@ def random_arm():
 
 
 def episode(epi=0):
-    reward = [0, 0, 0, 0, 0]
     gaus_step = 1
     exp_step = 1
     fair_step = 1
     random_step = 1
     pois_step = 1
+    reward = 0
     total_reward = 0
     for i in range(100):
         x = np.random.randint(0, 101)
@@ -60,70 +59,65 @@ def episode(epi=0):
                 action = np.random.randint(0, 5)
 
         if action == 0:
-            reward[0] = reward[0] + gaussian_arm()
+            reward = gaussian_arm()
+            expected_value[0] = (
+                expected_value[0] + (reward - expected_value[0]) / gaus_step
+            )
             gaus_step = gaus_step + 1
-
         elif action == 1:
-            reward[1] += exp_arm()
+            reward = exp_arm()
+            expected_value[1] = (
+                expected_value[1] + (reward - expected_value[1]) / exp_step
+            )
             exp_step = exp_step + 1
 
         elif action == 2:
-            reward[2] += poisson_arm()
+            reward = poisson_arm()
+            expected_value[2] = (
+                expected_value[2] + (reward - expected_value[2]) / pois_step
+            )
             pois_step = pois_step + 1
 
         elif action == 3:
-            reward[3] += fair_arm()
+            reward = fair_arm()
+            expected_value[3] = (
+                expected_value[3] + (reward - expected_value[3]) / fair_step
+            )
             fair_step = fair_step + 1
 
         elif action == 4:
-            reward[4] += random_arm()
+            reward = random_arm()
+            expected_value[4] = (
+                expected_value[4] + (reward - expected_value[4]) / random_step
+            )
             random_step = random_step + 1
 
         else:
             print("there is a problem with action allocation")
-
-    expected_value[4] = (
-        expected_value[4] + (reward[4] / random_step - expected_value[4]) / random_step
-    )
-    expected_value[3] = (
-        expected_value[3] + (reward[3] / fair_step - expected_value[3]) / fair_step
-    )
-    expected_value[2] = (
-        expected_value[2] + (reward[2] / pois_step - expected_value[2]) / pois_step
-    )
-    expected_value[1] = (
-        expected_value[1] + (reward[1] / exp_step - expected_value[1]) / exp_step
-    )
-    expected_value[0] = (
-        expected_value[0] + (reward[0] / gaus_step - expected_value[0]) / gaus_step
-    )
-    total_reward = reward[0] + reward[1] + reward[2] + reward[3] + reward[4]
+        total_reward = total_reward + reward
     average = total_reward / 100
     return average
 
 
 reward_history_epi = []
-expected_value = [1, 1, 1, 1, 1]
+expected_value = [0, 0, 0, 0, 0]
 for i in range(1000):
     reward = episode(0.1)
     reward_history_epi.append(reward)
-print(expected_value)
 
 
 reward_history = []
-expected_value = [1, 1, 1, 1, 1]
+expected_value = [0, 0, 0, 0, 0]
 for i in range(1000):
     reward = episode(0)
     reward_history.append(reward)
-print(expected_value)
 
 
-expected_value = [1, 1, 1, 1, 1]
+expected_value = [0, 0, 0, 0, 0]
 reward_history_epsilon = []
 for i in range(1000):
     reward = episode(0.1)
     reward_history_epsilon.append(reward)
-print(expected_value)
 
 
 fig, ax = plt.subplots()
